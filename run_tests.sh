2d14888d9dade78d17f84788bd95a9c3260c3010
santiago <sahernandezbo@unal.edu.co>
2016-10-20 21:15:32 -0700

    indices arreglados

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 nodes=$1
 trials=$2
-if [ -e "results.txt" ]; then
-    rm results.txt
+filename=$3
+echo "file $filename"
+if [ -e $filename ]; then
+    rm $filename
     echo "yes"
 else
     echo "no"
 fi
-echo "nodes = $nodes" >> results.txt
+echo "nodes = $nodes" >> $filename
 echo "trials = $trials"
 protocols=("OLSR" "AODV" "DSDV")
 mov=("Grid" "RandomDir" "RandomRect")
@@ -15,17 +17,18 @@ for i in $(seq 0 2)
 do
 	for j in $(seq 0 2)
 	do
-	    echo "${protocols[$i]} ${mov[$j]}" >> results.txt
+	    echo "${protocols[$i]} ${mov[$j]}"
+	    echo "${protocols[$i]} ${mov[$j]}" >> $filename
 	    for k in $(seq 1 $trials) 
 	    do	    
 	        rand=$RANDOM
 	        echo "rand = $rand"
 		    echo $rand
-		    str="$(./waf --run "flat --mobilityModel=$i --routing=$j --numNodes=$nodes --seed=$rand")"
+		    str="$(./waf --run "flat --mobilityModel=$j --routing=$i --numNodes=$nodes --seed=$rand")"
 		    #echo $str		    
 		    ans=$(echo $str | grep -oE "(([0-9]+(\.[0-9]+)?|-nan)[ ]?){6}")
 		    echo $ans
-		    echo $ans >> results.txt
+		    echo $ans >> $filename
 		    echo "out"
 		 done
 	done
